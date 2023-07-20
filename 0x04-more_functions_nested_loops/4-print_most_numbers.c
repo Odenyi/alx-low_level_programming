18f5b6a92fe99f435420fdfa70c03bb99e76bea6
odenyi <brianodenyi@gmail.com>
2023-07-20 10:47:33 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_most_numbers - print numbers from 0 - 9 except 2 and 4
  * Return: no return
@@ -8,7 +8,8 @@ void print_most_numbers(void)
 	char num;
 
 	for (num = '0'; num <= '9'; num++)
-		if ((num != '2') && (num != '4'))
-			_putchar(num);
+		if ((num == '2') || (num == '4'))
+			continue;
+		_putchar(num);
 	_putchar('\n');
 }
