b3976cadec5c9856d66ea468d7b8a4a314795b1f
odenyi <brianodenyi@gmail.com>
2023-07-20 10:57:02 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,8 @@ void print_most_numbers(void)
 	char num;
 
 	for (num = '0'; num <= '9'; num++)
-		if ((num != '2') || (num != '4'))
-			_putchar(num);
+		if ((num == '2') || (num == '4'))
+			continue;
+	_putchar(num);
 	_putchar('\n');
 }
